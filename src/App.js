418ca9e61904f8254e9300a09a7566c9501da824
Mr-Developer2204 <59203865+Mr-Developer2204@users.noreply.github.com>
2021-06-12 21:18:31 +0530

    Radio without styling is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import Radio from "./pages/index";
 
 function App() {
   return (
-    <div className="App">
-     <div>Hello World !</div>
-    </div>
+    <ChakraProvider>
+      <div>
+        <Box
+          w="100%"
+          h="1000px"
+          bgGradient={[
+            "linear(to-t, blue.200, teal.500)",
+            "linear(to-tr, teal.300,yellow.400)",
+            "linear(to-b, orange.100, purple.300)",
+          ]}>
+          <Radio />
+        </Box>
+      </div>
+    </ChakraProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Search from './pages/index';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
